890b55e943a1e7ac332292e7002cda5a547d3de3
vishesh <visheshbhagat7@yahoo.com>
2020-07-02 00:12:45 +0530

    initial commit

---
+++ app/src/main/java/com/vishesh/notekeeper/NoteKeeperData.kt
@@ -0,0 +1,8 @@
+package com.vishesh.notekeeper
+
+class CourseInfo(val courseId: String, val title: String)
+class NoteInfo(var course: CourseInfo, var title: String, var text: StringBuffer)
+
+
+
+
